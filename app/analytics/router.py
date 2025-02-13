cbdc776679389c4e78bb35ca9102f07f62b6fa13
Maksim Dreev <dreev24@yandex.ru>
2025-02-13 12:59:52 +0300

    analytics are ready

--- app/analytics/router.py
+++ app/analytics/router.py
@@ -3,16 +3,88 @@ from enum import Enum
 from typing import Annotated, Optional
 
 from fastapi import APIRouter, Depends, HTTPException, status
-from pydantic import BaseModel
-from sqlalchemy import select
+
+from sqlalchemy import func, select
 from sqlalchemy.orm import selectinload
 
-from app.database import Category, new_session, Operation
-from app.categories.router import CategoryType
+from app.categories.schemas import CategoryType
+from app.database import Category, Operation, new_session
+from app.operations.schemas import PeriodEnum
+
+router = APIRouter(prefix="/api/analytics", tags=["Аналитика"])
 
 
-router = APIRouter(
-    prefix="/api/analytics",
-    tags=["Аналитика"]
+@router.get("/", description="Выводит статистику по доходам и расходам на период")
+async def get_analitics(period: Optional[PeriodEnum] = None):
+    async with new_session() as session:
+        try:
+            if period is None:
+                start_date = date.today() - timedelta(days=364)
+            else:
+                start_date = date.today() - timedelta(days=period.value)
+
+            income_query = select(func.sum(Operation.amount)).where(
+                Operation.created_at >= start_date,
+                Operation.category.has(category_type="income"),
+            )
+            expense_query = select(func.sum(Operation.amount)).where(
+                Operation.created_at >= start_date,
+                Operation.category.has(category_type="expense"),
+            )
+
+            income_result = await session.execute(income_query)
+            expense_result = await session.execute(expense_query)
+
+            total_income = income_result.scalar() or 0
+            total_expenses = expense_result.scalar() or 0
+            cashflow = total_income - total_expenses
+
+            return {
+                "incomes": total_income,
+                "expenses": total_expenses,
+                "cashflow": cashflow,
+            }
+
+        except Exception as e:
+            raise e
+
+
+@router.get(
+    "/categories/", description="Выводит статистику по доходам\расходам на период ПО КАТЕГОРИЯМ"
 )
+async def get_analitics_by_category(
+    types: CategoryType,
+    period: Optional[PeriodEnum] = None,
+):
+    async with new_session() as session:
+        try:
+            if period is None:
+                start_date = date.today() - timedelta(days=364)
+            else:
+                start_date = date.today() - timedelta(days=period.value)
 
+            subquery = (
+                select(
+                    Operation.category_id,
+                    func.sum(Operation.amount).label("total_amount"),
+                )
+                .where(
+                    Operation.category.has(category_type=types),
+                    Operation.created_at >= start_date,
+                )
+                .group_by(Operation.category_id)
+                .subquery()
+            )
+
+
+            query = select(Category.name, subquery.c.total_amount).join(
+                subquery, Category.id == subquery.c.category_id
+            )
+
+            request = await session.execute(query)
+            result = request.all()
+
+            return [{"category": row[0], "total_amount": row[1]} for row in result]
+
+        except Exception as e:
+            raise HTTPException(status_code=400, detail=str(e))
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI
 from app.database import create_tables
 from app.categories.router import router as category_router
 from app.operations.router import router as operation_router
+from app.analytics.router import router as analytics_router
 
 
 @asynccontextmanager
@@ -25,7 +26,7 @@ async def index():
 
 app.include_router(category_router)
 app.include_router(operation_router)
-
+app.include_router(analytics_router)
 
 if __name__ == "__main__":
     import uvicorn
